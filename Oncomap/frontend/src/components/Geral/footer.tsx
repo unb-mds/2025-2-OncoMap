76e8dd483b43c6d40901aeb7e638605d4bff6f6b
Gotc2607 <giovanicoelho2005@gmail.com>
2025-09-25 15:45:31 -0300

    concertando os caminhos para os arquivos css

--- Oncomap/frontend/src/components/Geral/footer.tsx
+++ Oncomap/frontend/src/components/Geral/footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./style/Footer.css";
+import "../../style/Footer.css";
 
 const Footer = () => {
     return(
--- Oncomap/frontend/src/components/HomePage/navbar.tsx
+++ Oncomap/frontend/src/components/HomePage/navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link"; 
-import '../style/Navbar.css';
+import '../../style/Navbar.css';
       
 
 const Navbar = () =>{
