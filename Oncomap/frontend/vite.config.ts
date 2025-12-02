9116dd7df5fc0b7dd3b7c4c934f96699313f7e39
Gotc2607 <giovanicoelho2005@gmail.com>
2025-12-02 19:22:45 -0300

    feat: adicionando a configuração para testes no frontend

--- Oncomap/frontend/vite.config.ts
+++ Oncomap/frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,           // Permite usar describe, it, expect sem importar (igual ao Jest)
+    environment: 'jsdom',    // Simula o navegador para os componentes React
+    setupFiles: './src/setupTests.ts', // Arquivo onde importamos o jest-dom
+    css: true,               // (Opcional) Processa CSS nos testes, útil se usar módulos CSS
+  },
 })
