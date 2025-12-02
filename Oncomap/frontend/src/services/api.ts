a65dc49bc9feae002323d804fed7be59f3059f1a
Alexandroni07 <gaalexandroni@gmail.com>
2025-12-01 21:07:20 -0300

    Feat: add url correta para deploy

--- Oncomap/frontend/src/services/api.ts
+++ Oncomap/frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosInstance } from "axios";
 
 // Se estiver usando Vite, use import.meta.env.
 // Se for Create React App (CRA), use process.env.REACT_APP_API_URL
-const baseURL: string = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const baseURL: string = import.meta.env.VITE_API_URL || "https://oncomap-backend-pr8b.onrender.com";
 
 const api: AxiosInstance = axios.create({
   baseURL: baseURL,
